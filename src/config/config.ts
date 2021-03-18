5144c4728ded478b0cc7e8178dfb5893b4a36318
pactience <1079015171@qq.com>
2021-03-18 22:15:21 +0800

    docs(merge): merge

--- src/config/config.ts
+++ src/config/config.ts
@@ -22,9 +22,11 @@ type configType = {
 };
 
 const getDbUrl = () => {
+  const dbName = readLineSync.question("please enter DBname:");
   const userName = readLineSync.question("please enter db userName:");
   const password = readLineSync.question("please enter db password:");
-  return `mongodb://${userName}:${password}@1.15.54.144:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
+
+  return `mongodb://${userName.trim()}:${password.trim()}@1.15.54.144:27017/?authSource=${dbName.trim()}&readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 };
 
 const config: configType = {
