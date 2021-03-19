fb517eeabe6d4f7f1b2aea679fecff3294578863
pactience <1079105171@qq.com>
2021-03-19 18:33:33 +0800

    feat(优化上传图片问题): 上传图片优化

--- src/controller/common/upload/upload.ts
+++ src/controller/common/upload/upload.ts
@@ -3,8 +3,6 @@ import { InitRoutersType } from "../../../global";
 import UploadFile from "../../../common/upload-file/upload-file";
 import Response from "../../../base-response/base-response";
 
-
-
 /** 初始化上传文件router */
 const InitUploadRouter: InitRoutersType = (koa, router, client) => {
   router.post("上传图片", "/upload/image", async (ctx, next) => {
@@ -13,7 +11,10 @@ const InitUploadRouter: InitRoutersType = (koa, router, client) => {
     } = ctx;
     const data = await UploadFile(files, "\\upload\\image");
 
-    ctx.body = Response.baseResponse(data.fileName);
+    ctx.body = Response.baseResponse({
+      fileName: data.fileName,
+      filePath: data.filePath,
+    });
   });
 };
 
