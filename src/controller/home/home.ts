a221bf4181c8af88b114a27423d60be725237d3f
pactience <1079105171@qq.com>
2021-03-19 13:43:10 +0800

    docs(修改类型声明注释): 类型声明注释

--- src/controller/home/home.ts
+++ src/controller/home/home.ts
@@ -10,18 +10,15 @@ import Data from "./data";
 /** 初始化Home相关的路由 */
 const InitHomeRouters: InitRoutersType = (koa, router, client) => {
   router.get("首页列表", "/home", async (ctx) => {
-    console.log(ctx.req);
     const db = client.db(config.db);
     const collect = db.collection<Data.HomeItem>(config.collections.home);
     const list: Data.HomeItem[] = [];
-    await collect
-      .find()
-      .forEach((item) =>
-        list.push({
-          ...item,
-          image: `http://gsea.top/${item.image}`,
-        })
-      );
+    await collect.find().forEach((item) =>
+      list.push({
+        ...item,
+        image: `http://gsea.top/${item.image}`,
+      })
+    );
     ctx.body = Response.baseResponse(list);
   });
 
